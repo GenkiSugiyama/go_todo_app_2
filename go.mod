1e1504aed5ca82ab0b56f8fbab94f9858a9fa6af
杉山 元基 <sgym0318marcos@gmail.com>
2026-02-22 16:38:48 +0900

    fix: server refactor & implement test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GenkiSugiyama/go_todo_app_2
 
 go 1.25.4
+
+require golang.org/x/sync v0.19.0
--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v\n", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v\n", err)
-		os.Exit(1)
 	}
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// s.ListenAndServe()が終了すると、eg.Go()の処理も終了する
+	eg.Go(func() error {
+		// s.Shutdown()が呼び出されると、s.ListenAndServe()が終了する
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close server: %+v\n", err)
+			return err
+		}
+		return nil
+	})
+
+	// Contextによるキャンセル通知が発生するまで処理を待機する
+	<-ctx.Done()
+	// Contextによるキャンセル通知が発生したら、サーバーをシャットダウンする
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown server: %+v\n", err)
+	}
+
+	// eg.Go()メソッドが終了するのを待機して、エラーがあれば返す
+	return eg.Wait()
 }
