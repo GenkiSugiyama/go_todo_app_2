b6625b1db63fdb88912422b1e200062f6a44e044
杉山 元基 <sgym0318marcos@gmail.com>
2026-03-01 14:37:43 +0900

    fix: read from envfile

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/GenkiSugiyama/go_todo_app_2
 go 1.25.4
 
 require golang.org/x/sync v0.19.0
+
+require github.com/caarlos0/env/v6 v6.10.1 // indirect
--- main.go
+++ main.go
@@ -8,26 +8,29 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/GenkiSugiyama/go_todo_app_2/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to run: %v\n", err)
 		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v\n", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
-		log.Printf("failed to terminate server: %v\n", err)
 	}
 }
 
 // 引数にListenerを受け取るようにする
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v\n", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v\n", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
