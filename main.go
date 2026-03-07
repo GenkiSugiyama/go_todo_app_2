cc9ab8fd5f1cc30cba6318b9759e9f29fa331b17
杉山 元基 <sgym0318marcos@gmail.com>
2026-03-07 16:12:10 +0900

    fix: refactor server

--- main.go
+++ main.go
@@ -15,6 +15,47 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type Server struct {
+	srv *http.Server
+	l   net.Listener
+}
+
+func NewServer(l net.Listener, mux http.Handler) *Server {
+	return &Server{
+		srv: &http.Server{Handler: mux},
+		l:   l,
+	}
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	// 1. graceful shutdownのためにシグナルを受け取るContextを作成する
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// *errgroup.Group型を使ってエラーを含むゴルーチン間の並行処理を実装する
+	eg, ctx := errgroup.WithContext(ctx)
+	// 2. eg.Go()を使ってfunc() errorの処理を別ゴルーチンで実行する（サーバー起動）
+	eg.Go(func() error {
+		// 5. s.srv.Shutdown()が呼び出されると、s.srv.Serve()が終了する
+		if err := s.srv.Serve(s.l); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v\n", err)
+			return err
+		}
+		return nil
+	})
+
+	// 3. キャンセル通知が発生するまで処理を待機する
+	<-ctx.Done()
+	// 4. ctx経由でキャンセルが通知されたら、サーバーをシャットダウンする
+	if err := s.srv.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v\n", err)
+	}
+
+	// 6. eg.Go()メソッドが終了するのを待機して、eg.Go()内のメソッドでエラーがあれば返す
+	return eg.Wait()
+}
+
 func main() {
 	if err := run(context.Background()); err != nil {
 		log.Printf("failed to run: %v\n", err)
