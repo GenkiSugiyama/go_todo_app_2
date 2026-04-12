b1a56b434098020668a1fa060a9a134f5c7a8e1d
杉山 元基 <sgym0318marcos@gmail.com>
2026-04-12 15:44:37 +0900

    done: complete chapter17

--- go.mod
+++ go.mod
@@ -2,15 +2,18 @@ module github.com/GenkiSugiyama/go_todo_app_2
 
 go 1.25.4
 
-require golang.org/x/sync v0.20.0
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/go-playground/validator/v10 v10.30.2
+	github.com/google/go-cmp v0.7.0
+	golang.org/x/sync v0.20.0
+)
 
 require (
-	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.30.2 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/crypto v0.49.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
--- mux.go
+++ mux.go
@@ -1,14 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/GenkiSugiyama/go_todo_app_2/handler"
+	"github.com/GenkiSugiyama/go_todo_app_2/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
 
 // どんなハンドラー実装をどのパスで受け付けるかのルーティングを定義する関数
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
-	// /healthにアクセスが来たら、JSON形式で{"status":"ok"}を返すハンドラー関数を登録する
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	})
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
 	return mux
 }
